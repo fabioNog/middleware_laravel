624b939cb5a1a79ee2e047c346128c680f4a911b
fabioNog <fabio.carmo@computacao.ufla.br>
2020-06-15 21:33:19 -0300

    Nomeando um middleware e adicionando a controller pelo metodo construtor

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
+    function __construct()
+    {
+        $this->middleware('primeiro');
+    }
     function index(){
         return
             "<h3>Lista de Usuario</h3>
